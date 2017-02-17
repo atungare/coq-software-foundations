9c0ec283fe4bb260850186f0f229c569049120c2
atungare <ajay.tungare@gmail.com>
2017-02-17 01:34:31 -0500

    even more induction

--- Induction.v
+++ Induction.v
@@ -9,8 +9,6 @@ Proof.
   - simpl. rewrite <- IHn'. reflexivity.
 Qed.
 
- Print LoadPath.
-
 Theorem minus_diag: forall n:nat,
   n - n = O.
 Proof.
@@ -140,8 +138,92 @@ Proof.
       reflexivity.
 Qed.
 
-
 (* exercises *)
+Theorem leb_refl : forall n:nat,
+  true = leb n n.
+Proof.
+  intros.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem zero_nbeq_S : forall n:nat,
+  beq_nat 0 (S n) = false.
+Proof.
+  intros.
+  destruct n as [| n'].
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem andb_false_r : forall b : bool,
+  andb b false = false.
+Proof.
+  intros.
+  destruct b as [| b'].
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem plus_ble_compat_l : forall n m p : nat,
+  leb n m = true -> leb (p + n) (p + m) = true.
+Proof.
+  intros.
+  induction p as [| p' IHp'].
+  - simpl. rewrite -> H. reflexivity.
+  - simpl. rewrite -> IHp'. reflexivity.
+Qed.
+
+Theorem S_nbeq_0 : forall n:nat,
+  beq_nat (S n) 0 = false.
+Proof.
+  intros.
+  replace (S n) with (n + 1).
+  - rewrite -> plus_1_neq_0. reflexivity.
+  - rewrite <- plus_1_r. reflexivity.
+Qed.
+
+Theorem mult_1_l : forall n:nat, 1 * n = n.
+Proof.
+  intros.
+  destruct n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite <- plus_O_r. reflexivity.
+Qed.
+
+Theorem all3_spec : forall b c : bool,
+    orb
+      (andb b c)
+      (orb (negb b)
+               (negb c))
+  = true.
+Proof.
+  intros.
+  destruct b as [| b'].
+  destruct c as [| c'].
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem mult_plus_distr_r : forall n m p : nat,
+  (n + m) * p = (n * p) + (m * p).
+Proof.
+  intros.
+  induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'. rewrite -> plus_assoc. reflexivity.
+Qed.
+
+Theorem mult_assoc : forall n m p : nat,
+  n * (m * p) = (n * m) * p.
+Proof.
+  intros.
+  induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'. rewrite -> mult_plus_distr_r. reflexivity.
+Qed.
 
 Theorem beq_nat_refl : forall n : nat,
   true = beq_nat n n.
